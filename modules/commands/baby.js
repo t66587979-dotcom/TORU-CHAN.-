40160dc381e0a3e0d5429f22d9c15d7c8328876a
hridoyhossen48360 <alaminkhan123466hh@gmail.com>
2026-02-10 03:39:50 +0600

    Update baby.js

--- modules/commands/baby.js
+++ modules/commands/baby.js
@@ -179,7 +179,7 @@ module.exports.handleEvent = async function ({ api, event, Users }) {
  if (!text || !simsim) return;
 
  const senderName = await Users.getNameUser(event.senderID);
- const triggers = ["baby", "bby", "xan", "bbz", "toru", "kakashi"];
+ const triggers = ["baby", "bby", "xan", "bot", "toru", "kakashi"];
 
  if (triggers.includes(text)) {
  const replies = [
@@ -216,7 +216,7 @@ module.exports.handleEvent = async function ({ api, event, Users }) {
  });
  }
 
- const matchPrefix = /^(baby|bby|xan|bbz|toru|kakashi)\s+/i;
+ const matchPrefix = /^(baby|bby|xan|bot|toru|kakashi)\s+/i;
  if (matchPrefix.test(text)) {
  const query = text.replace(matchPrefix, "").trim();
  if (!query) return;
