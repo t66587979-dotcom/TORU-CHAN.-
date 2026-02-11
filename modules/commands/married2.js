23fefb39c68d1acfaf3066c1df584fab33ebd9ce
t66587979-dotcom <t66587979@gmail.com>
2026-02-11 13:41:34 +0600

    Update married2.js

--- modules/commands/married2.js
+++ modules/commands/married2.js
@@ -4,7 +4,7 @@ module.exports.config = {
     hasPermssion: 0,
     credits: "—͟͟͞͞𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
     description: "married",
-    commandCategory: "img",
+    commandCategory: "Tag Fun",
     usages: "[@mention]",
     cooldowns: 5,
     dependencies: {
